cf1939345c1e77d15aa04b60046d492b94e94d76
Agustin Cassani <agustinscassani@gmail.com>
2025-05-23 10:38:46 -0300

    Initial scaffolding added.

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from 'next/font/google';
+import type { Metadata } from 'next';
+
+import './globals.css';
+
+const geistSans = Geist({
+  subsets: ['latin'],
+  variable: '--font-geist-sans',
+});
+
+const geistMono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-geist-mono',
+});
+
+export const metadata: Metadata = {
+  description: 'Luxury apartments in Mendoza for rent.',
+  title: 'Mendoza Apartments',
+};
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  // Render
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
